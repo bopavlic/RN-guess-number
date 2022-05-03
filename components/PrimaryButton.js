fcec8e0fb76bac7271ce5c04d249961430f4c47d
Borna Pavlić <borna.pavlic@yahoo.com>
2022-05-03 11:43:01 +0200

    feat-added start screen look

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,13 +1,47 @@
-import { View, Text } from 'react-native';
 import React from 'react';
+import { View, Text, Pressable, StyleSheet } from 'react-native';
 
 const PrimaryButton = (props) => {
   const { children } = props;
+
+  const handlePress = () => {};
+
   return (
-    <View>
-      <Text>{children}</Text>
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed] // iphone
+            : styles.buttonInnerContainer
+        }
+        onPress={handlePress}
+        android_ripple={{ color: '#72063c' }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
     </View>
   );
 };
 
 export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: 'hidden',
+  },
+  buttonInnerContainer: {
+    backgroundColor: '#de1f7e',
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
